c070a429f604b52e37a61f8c6e9be6c4797bc388
Aleksandr Khmyrov <sa3@mail.ru>
2022-12-30 19:18:59 +0800

    Добавил две клопки для уведомлений

--- Novigation/Service layer/LocalNotificationsService.swift
+++ Novigation/Service layer/LocalNotificationsService.swift
@@ -12,7 +12,7 @@ import UserNotificationsUI
 
 
 
-class LocalNotificationsService {
+class LocalNotificationsService: NSObject {
 
 
     let unCenter = UNUserNotificationCenter.current()
@@ -20,7 +20,9 @@ class LocalNotificationsService {
 
     func registerForLatestUpdatesIfPossible(completionHandler: @escaping (String, LocalNotificationsService?) -> Void) {
 
-        self.unCenter.requestAuthorization(options: [.sound, .badge, .provisional ] ) { success, error in
+        self.registerUpdatesCategory()
+
+        self.unCenter.requestAuthorization(options: [.sound, .badge ] ) { success, error in
 
             if let error {
 
@@ -30,30 +32,90 @@ class LocalNotificationsService {
 
             else {
 
-                    print(String(success))
-                    completionHandler(String(success), self)
+                print(String(success))
+                completionHandler(String(success), self)
 
-                    let content = UNMutableNotificationContent()
-
-                    content.badge = 1
-                    content.title = "Напоменаем"
-                    content.body = "Посмотрите последние обновления"
-
-                    var dateComponents = DateComponents()
-                    dateComponents.hour = 19
-                    dateComponents.minute = 00
-
-
-                    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-              //      let triggerTwo = UNTimeIntervalNotificationTrigger(timeInterval: 9, repeats: false)
-
-                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger )
-
-                    self.unCenter.add(request)
-
+                self.addNotificationRequest(timer: false)
 
             }
         }
     }
+
+
+
+    private func addNotificationRequest(timer: Bool) {
+
+        let content = UNMutableNotificationContent()
+
+        content.badge = 1
+        content.title = "Напоменаем"
+        content.body = "Посмотрите последние обновления"
+        content.categoryIdentifier = "updates"
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = 19
+        dateComponents.minute = 18
+
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+        if timer {
+
+            let triggerTimer = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: triggerTimer)
+
+            self.unCenter.add(request)
+
+        }
+
+        else {
+
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger )
+
+            self.unCenter.add(request)
+
+        }
+    }
+
+
+
+    func registerUpdatesCategory() {
+
+        self.unCenter.delegate = self
+
+        let actionButtonRemove = UNNotificationAction(identifier: "actionButtonRemove", title: "Отключить это уведомление", options: .destructive)
+
+        let actionButtonRepeat = UNNotificationAction(identifier: "actionButtonRepeat", title: "Напомнить через 5 секунд")
+
+        let category = UNNotificationCategory(identifier: "updates", actions: [actionButtonRemove, actionButtonRepeat ], intentIdentifiers: [])
+
+        self.unCenter.setNotificationCategories([category])
+    }
+
 }
+
+
+extension LocalNotificationsService: UNUserNotificationCenterDelegate {
+
+
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        switch response.actionIdentifier {
+
+        case "actionButtonRemove":
+            self.unCenter.removePendingNotificationRequests(withIdentifiers: [response.notification.request.identifier])
+
+        case "actionButtonRepeat":
+            self.addNotificationRequest(timer: true)
+
+        default:
+            print("кнопка дефолт")
+        }
+
+        completionHandler()
+    }
+}
+
+
